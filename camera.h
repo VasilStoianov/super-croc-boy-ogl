996d0b84f9dcf4685bb70b2f94eb1cac0b71da45
lays <vaseto.stoianov.94@gmail.com>
2025-05-01 11:03:55 +0300

    camera

--- camera.h
+++ camera.h
@@ -5,22 +5,21 @@
 #include "shader.h"
 
 typedef struct {
-    mat4f matrix;
-    vector position;
+  mat4f matrix;
+  vector position;
 } Camera;
 
-Camera* create_camera(){
-    Camera * camera = (Camera*) malloc(sizeof(Camera));
-    camera->matrix =  identity();
-    camera->position = (vector){.x = 0.f,.y = 0.f};
-    return camera;
+Camera *create_camera() {
+  Camera *camera = (Camera *)malloc(sizeof(Camera));
+  camera->matrix = identity();
+  camera->position = (vector){.x = 0.f, .y = 0.f};
+  return camera;
 }
 
-void set_camera(Camera *camera,int shaderId){
+void set_camera(Camera *camera, int shaderId) {
 
-        setTranslation(&(camera->matrix) ,camera->position );
-    set_matrix_uniform(camera->matrix, shaderId, "camera");
-
+  setTranslation(&(camera->matrix), camera->position);
+  set_matrix_uniform(camera->matrix, shaderId, "camera");
 }
 
 #endif
